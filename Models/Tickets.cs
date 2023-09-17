043dbd5efe27d0961414529f3991d556b41a81cb
JeanRD7 <jean64098@gmail.com>
2023-09-17 18:46:38 -0400

    Removiendo la limitacion de caracteres a ClienteID y a SistemaID

--- Models/Tickets.cs
+++ Models/Tickets.cs
@@ -9,10 +9,10 @@ namespace Registro_de_Tickets.Models
         public int TicketId { get; set; }
 
         public DateTime Fecha { get; set; } = DateTime.Now;
-        [Required(ErrorMessage = "No puede dejar el campo vacio"), MaxLength(3)]
-        public int ClienteId { get; set; }
-        [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(3)]
-        public int SistemaId { get; set; }
+        [Required(ErrorMessage = "No puede dejar el campo vacio")]
+        public int? ClienteId { get; set; }
+        [Required(ErrorMessage = "No puede dejar el campo vacio")]
+        public int? SistemaId { get; set; }
         [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(15)]
         public string? SolicitadoPor { get; set; }
         [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(20)]

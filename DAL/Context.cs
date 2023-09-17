731798d07f2496fb035e4255860c35d2ad54f36f
JeanRD7 <jean64098@gmail.com>
2023-09-17 00:22:58 -0400

    Creacion del Contexto

---
+++ DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_de_Tickets.Models;
+
+namespace Registro_de_Tickets.DAL 
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context>op) : base(op) { }
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}

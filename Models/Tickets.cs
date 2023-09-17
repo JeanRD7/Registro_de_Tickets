7477273e26d444e135a5876a827b32f8dc4331a2
JeanRD7 <jean64098@gmail.com>
2023-09-17 18:05:49 -0400

    Colocando limites de caracteres en el modelo

--- Models/Tickets.cs
+++ Models/Tickets.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using FluentValidation;
 
 namespace Registro_de_Tickets.Models
 {
@@ -10,15 +9,15 @@ namespace Registro_de_Tickets.Models
         public int TicketId { get; set; }
 
         public DateTime Fecha { get; set; } = DateTime.Now;
-
-        public int ClienteId { get; set; }
-        [Required (ErrorMessage ="No puede dejar el campo vacio"), MaxLength(3)]
-        public int SistemaId { get; set;}
         [Required(ErrorMessage = "No puede dejar el campo vacio"), MaxLength(3)]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(3)]
+        public int SistemaId { get; set; }
+        [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(15)]
         public string? SolicitadoPor { get; set; }
-        [Required(ErrorMessage = "No puede dejar el campo vacio")]
+        [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(20)]
         public string? Asunto { get; set; }
-        [Required(ErrorMessage = "No puede dejar el campo vacio")]
+        [Required(ErrorMessage = "No puede dejar el campo vacio"),MaxLength(30)]
         public string? Descripcion { get; set; }
 
     }

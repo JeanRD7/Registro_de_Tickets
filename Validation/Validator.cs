ce555cd2c3f8ef9927442b17c44ffbefc5e3d91e
JeanRD7 <jean64098@gmail.com>
2023-09-17 00:25:03 -0400

    creacion de la base de datos

---
+++ Validation/Validator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using Registro_de_Tickets.Models;
+namespace Registro_de_Tickets.Validation
+{
+    public class TicketValidator : AbstractValidator<Tickets>
+    {
+        int limite = 7;
+        public TicketValidator() 
+        {
+            RuleFor(op => op.ClienteId)
+                .NotEmpty()
+                ;
+
+        }
+
+        
+    }
+}

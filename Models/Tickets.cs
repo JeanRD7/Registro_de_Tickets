fa81b77c51b3f45f00b74803373b06e97a417671
JeanRD7 <jean64098@gmail.com>
2023-09-17 00:22:37 -0400

    Creacion del Modelo

---
+++ Models/Tickets.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace Registro_de_Tickets.Models
+{
+    public class Tickets
+    {
+        [Key]
+
+        public int TicketId { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public int ClienteId { get; set; }
+        [Required (ErrorMessage ="No puede dejar el campo vacio"), MaxLength(3)]
+        public int SistemaId { get; set;}
+        [Required(ErrorMessage = "No puede dejar el campo vacio"), MaxLength(3)]
+        public string? SolicitadoPor { get; set; }
+        [Required(ErrorMessage = "No puede dejar el campo vacio")]
+        public string? Asunto { get; set; }
+        [Required(ErrorMessage = "No puede dejar el campo vacio")]
+        public string? Descripcion { get; set; }
+
+    }
+}
